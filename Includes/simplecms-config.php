b901cd5f017b98b80ccbdb10248aaac4fda03b11
lborocold <dongleng86@gmail.com>
2016-11-08 15:31:31 +0000

    first commit
    test

---
+++ Includes/simplecms-config.php
@@ -0,0 +1,9 @@
+<?php
+    define('DB_NAME', 'simplecms698');
+    define('DB_USER', 'simplecmsuser698');
+    define('DB_PASSWORD', 'nT1[AuZ^#LTO');
+    define('DB_HOST', 'localhost');
+
+    define('DEFAULT_ADMIN_USERNAME', 'admin');
+    define('DEFAULT_ADMIN_PASSWORD', 'gerrard8');
+?>
